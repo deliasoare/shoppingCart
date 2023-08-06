ad1ab4cbdb8f6a1e2a5fdc9a6deb6601b78cfff1
deliasoare <deliasoare006@yahoo.com>
2023-08-07 00:05:19 +0300

    Enhancing the style of the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,22 +8,21 @@ const Footer = () => {
             <button onClick={backToTop}>Back to top</button>
             <div className="footerInfo">
                 <div className='aboutUs'>
-                    <a href="" className="header aboutUsHeader">About us</a>
+                    <a href="" className="header aboutUsHeader"><b>A</b>bout us</a>
                     <a href="">Company info</a>
                     <a href="">Investors</a>
                     <a href="">Careers</a>
                     <a href="">Policies</a>
                 </div>
                 <div className='followUs'>
-                    <a href="" className="header followUsHeader">Follow us</a>
+                    <a href="" className="header followUsHeader"><b>F</b>ollow us</a>
                     <a href="">Newsletter</a>
                     <a href="">TikTok</a>
                     <a href="">Instagram</a>
                     <a href="">Facebook</a>
-                    <a href="">Youtube</a>
                 </div>
                 <div className='help'>
-                    <a href="" className="header helpHeader">Help</a>
+                    <a href="" className="header helpHeader"><b>H</b>elp</a>
                     <a href="">My MT account</a>
                     <a href="">Shipping</a>
                     <a href="">Payment and invoices</a>
