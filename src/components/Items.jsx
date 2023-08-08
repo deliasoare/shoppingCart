9bd642c6a4d90f4616054845db50203c507fa61f
deliasoare <deliasoare006@yahoo.com>
2023-08-08 19:18:57 +0300

    Add a few of the product details + develop a read-only star rating system

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -22,16 +22,51 @@ const Items = () => {
             throw new Error('this ain\'t good');
     }
 
+    const rating = (rating) => {
+        let colors = [
+            '#dc2626',
+            '#f87171',
+            '#facc15',
+            '#bef264',
+            '#4d7c0f'
+        ]
+        let that = ([...Array(5)]).map((star, starIndex) => {
+            console.log(starIndex, parseInt(rating));
+            if (starIndex < parseInt(rating)) {
+                return (
+                    <div style={{background: colors[parseInt(rating)]}} className='star fullStar'>
+                        <span>★</span>
+                    </div>
+                );
+            }
+            else if (starIndex === parseInt(rating)) {
+                let nr = String(rating).split("");
+                let decimalPoint = Number(nr[2]) ? Number([nr[2]]) : 0;
+                console.log(decimalPoint * 10);
+                return (
+                    <div style={{background: `linear-gradient(to right,${colors[parseInt(rating)]} ${decimalPoint * 10}%, 0%, #bbbac0 ${100 - (decimalPoint * 10)}%)`}} className='star halfStar'>
+                        <span>★</span>
+                    </div>
+                );
+            }
+            else
+                return (
+                    <div style={{color: 'white', background: '#bbbac0'}} className='star emptyStar'>
+                        <span>★</span>
+                    </div>
+                );
+        })
+        return that;
+    }
+    
     useEffect(() => {
         setCategoryProducts([]);
         if (category !== 'All Items') {
-            console.log('running');
             products.forEach(product => {
                 if (product.category === category) 
                     setCategoryProducts(prevProd => prevProd.concat(product));
             })
         }
-        console.log(categoryProducts)
     }, [category])
 
 
@@ -44,11 +79,34 @@ const Items = () => {
             <div className='categoryItems'>
                 {category === 'All Items' ?
                     products.map(product => {
-                        return <p>{product.title}</p>
+                        console.log(product);
+                        return (
+                            <div className='item'>
+                                <div className='itemImage'>
+                                    <img src={product.image}/>  
+                                </div>
+                                <div className="itemTitle">{product.title}</div>
+                                <div className='itemRating'>
+                                    <div className="stars">{rating(product.rating.rate)}</div>
+                                </div>
+                                <div className="itemPrice">${product.price}</div>
+                            </div>
+                        );
                     })
                     :
                     categoryProducts.map(product => {
-                        return <p>{product.title}</p>
+                        return (
+                            <div className='item'>
+                                <div className='itemImage'>
+                                    <img src={product.image}/>  
+                                </div>
+                                <div className="itemTitle">{product.title}</div>
+                                <div className='itemRating'>
+                                    <div className="stars">{rating(product.rating.rate)}</div>
+                                </div>
+                                <div className="itemPrice">${product.price}</div>
+                            </div>
+                        );
                     })
                 }
             </div>
